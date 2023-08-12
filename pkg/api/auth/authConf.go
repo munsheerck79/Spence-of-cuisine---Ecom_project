d3fc4b70e4ea27e1388911b8438e2056ff43e6d7
munsheer ck <munsheerck98@gmail.com>
2023-08-12 09:40:25 +0530

    debug

--- pkg/api/auth/authConf.go
+++ pkg/api/auth/authConf.go
@@ -81,12 +81,12 @@ func TwilioSendOTP(phoneNumber string) (string, error) {
 	SERVICESID = config.GetConfig().SERVICESID
 	ACCOUNTSID = config.GetConfig().ACCOUNTSID
 	AUTHTOKEN = config.GetConfig().AUTHTOKEN
-	// // ACCOUNTSID = "AC2c8bf06da44f10b978038088c472580e"
-	// // AUTHTOKEN = "9ac2047bdc66673ca4d6703303a33f0a"
-	// // SERVICESID = "VA61d4b739330f391e167f7b0cad47d861"
-	// fmt.Println("ser", SERVICESID)
-	// fmt.Println("acc", ACCOUNTSID)
-	// fmt.Println("auth", AUTHTOKEN)
+	ACCOUNTSID = "AC2c8bf06da44f10b978038088c472580e"
+	AUTHTOKEN = "9ac2047bdc66673ca4d6703303a33f0a"
+	SERVICESID = "VA61d4b739330f391e167f7b0cad47d861"
+	fmt.Println("ser", SERVICESID)
+	fmt.Println("acc", ACCOUNTSID)
+	fmt.Println("auth", AUTHTOKEN)
 	client = twilio.NewRestClientWithParams(twilio.ClientParams{
 		Password: AUTHTOKEN,
 		Username: ACCOUNTSID,
@@ -104,7 +104,7 @@ func TwilioSendOTP(phoneNumber string) (string, error) {
 	resp, err := client.VerifyV2.CreateVerification(SERVICESID, params)
 
 	if err != nil {
-		fmt.Println("error at twilio")
+		fmt.Println("error at twilio", err)
 		return "", err
 	}
 	fmt.Println("return from auth")
